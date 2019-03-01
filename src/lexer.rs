af008e0d737f37e3919bcc918b790a266eb3bdc7
anak10thn <anak10thn@gmail.com>
2019-03-01 22:54:07 +0700

    FIX: lexer structure

---
+++ src/lexer.rs
@@ -0,0 +1,41 @@
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "baik.pest"]
+struct BaikLexer;
+use Rule::*;
+
+pub fn lexer() {
+    let pairs = BaikLexer::parse(expr, "1234+1").unwrap_or_else(|e| panic!("{}", e));
+
+    for pair in pairs {
+        println!("Rule:    {:?}", pair.as_rule());
+        // println!("Span:    {:?}", pair.as_span());
+        // println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                whitespace => println!("Letter:  {}", inner_pair.as_str()),
+                int => println!("Digit:   {}", inner_pair.as_str()),
+                unicode => println!("Unicode:   {}", inner_pair.as_str()),
+                string => println!("String:   {}", inner_pair.as_str()),
+                hex => println!("Hex:   {}", inner_pair.as_str()),
+                escape => println!("Escape:   {}", inner_pair.as_str()),
+                tulis => println!("Tulist:   {}", inner_pair.as_str()),
+                document => println!("Document:   {}", inner_pair.as_str()),
+                letters => println!("Letters:   {}", inner_pair.as_str()),
+                expr => println!("Expr:   {}", inner_pair.as_str()),
+                operation => println!("Operation:   {}", inner_pair.as_str()),
+                term => println!("Term:   {}", inner_pair.as_str()),
+                add => println!("aritmatik:   {}", inner_pair.as_str()),
+                subtract => println!("aritmatik:   {}", inner_pair.as_str()),
+                multiply => println!("aritmatik:   {}", inner_pair.as_str()),
+                divide => println!("aritmatik:   {}", inner_pair.as_str()),
+                power => println!("aritmatik:   {}", inner_pair.as_str()),
+                float => println!("Float    :   {}", inner_pair.as_str()),
+                _ => unreachable!()
+            };
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,10 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
+mod lexer;
 
-#[derive(Parser)]
-#[grammar = "grammer.pest"]
-struct BaikLexer;
+use lexer::lexer;
 
 fn main() {
-    let pairs = BaikLexer::parse(Rule::document, "tulis 'hallo',1234,'yaya\t' ").unwrap_or_else(|e| panic!("{}", e));
-
-    for pair in pairs {
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::whitespace => println!("Letter:  {}", inner_pair.as_str()),
-                Rule::int => println!("Digit:   {}", inner_pair.as_str()),
-                Rule::unicode => println!("Unicode:   {}", inner_pair.as_str()),
-                Rule::string => println!("String:   {}", inner_pair.as_str()),
-                Rule::hex => println!("Hex:   {}", inner_pair.as_str()),
-                Rule::escape => println!("Escape:   {}", inner_pair.as_str()),
-                Rule::tulis => println!("Tulist:   {}", inner_pair.as_str()),
-                Rule::document => println!("Document:   {}", inner_pair.as_str()),
-                Rule::letters => println!("Letters:   {}", inner_pair.as_str()),
-                _ => unreachable!()
-            };
-        }
-    }
+    lexer();
 }
